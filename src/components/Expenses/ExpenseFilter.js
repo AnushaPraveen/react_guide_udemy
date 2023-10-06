df859330266e23b68e9ec4480c0c22c88eca737d
AnushaPraveen <annedianu.12@gmail.com>
2023-10-06 12:20:35 -0600

    styling filter

--- src/components/Expenses/ExpenseFilter.js
+++ src/components/Expenses/ExpenseFilter.js
@@ -1,11 +1,10 @@
-import React , {useState} from 'react';
+import React from 'react';
 
 import './ExpenseFilter.css';
 
 const ExpensesFilter = (props) => {
-  //const [year, setYear] = useState('');
 
-  const yearSelected = (event) =>
+  const dropdownchangeHandler = (event) =>
   {
     props.onYearFilter(event.target.value);
   };
@@ -14,7 +13,7 @@ const ExpensesFilter = (props) => {
     <div className='expenses-filter'>
       <div className='expenses-filter__control'>
         <label>Filter by year</label>
-        <select onChange={yearSelected}>
+        <select onChange={dropdownchangeHandler} value={props.selected}>
           <option value='2022'>2022</option>
           <option value='2021'>2021</option>
           <option value='2020'>2020</option>
